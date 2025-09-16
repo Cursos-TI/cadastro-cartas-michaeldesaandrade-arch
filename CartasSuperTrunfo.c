5cacdabb47e8e8551e66b10357f616580bfb7a80
michaeldesaandrade-arch <michaeldesaandrade@gmail.com>
2025-09-16 20:35:46 -0300

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,11 +5,33 @@
 // Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
 
 int main() {
-  // Área para definição das variáveis para armazenar as propriedades das cidades
 
-  // Área para entrada de dados
+  int populacao, pontosturisticos;
+  char carta, estado, cidade;
+  float area, pib;
+    
+    printf ("digite a carta\n");
+    scanf ("%s", &carta);
+    
+    printf ("digite o estado\n");
+    scanf ("%s", &estado);
+    
+    printf ("digite a cidade\n");
+    scanf ("%s", &cidade);
+    
+    printf ("digite a população\n");
+    scanf ("%d", &populacao);
+    
+    printf ("digite o número de pontos turisticos\n");
+    scanf ("%d", &pontosturisticos);
+    
+    printf ("digite a área\n");
+    scanf ("%f", &area);
+    
+    printf ("digite o pib\n");
+    scanf ("%f", &pib);
 
-  // Área para exibição dos dados da cidade
+}
 
 return 0;
 } 
